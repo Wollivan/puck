6fef5ad12b7cdb11fff28533c8619674aa24e534
Tim Smith <tim.smith2814@gmail.com>
2024-10-29 23:37:39 +0000

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,8 @@ shareBtn.addEventListener("click", async () => {
   try {
     await navigator.share({
       title: "PRINT THIS FOR ME!\n",
-      text: "\nI found a game I want to play, please can you print me a copy? The website said to print with 100% infill whatever that means, but they sound smart, you should listen to them!",
-      url: "https://github.com/Wollivan/puck/tree/main/puck",
+      text: "Hey friend with a 3D printer...\nI found a game I want to play, please can you print me a copy? I'll let you win the first game!",
+      url: "https://puck.wollivan.dev",
     });
     console.log("Data was shared successfully");
   } catch (err) {
